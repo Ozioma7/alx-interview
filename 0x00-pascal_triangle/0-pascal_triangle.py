ead62e74f2a16bb392af80fbe657b68e8377616f
Ozioma7 <okonisaozioma@gmail.com>
2024-03-12 13:51:02 +0100

    formatted it

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Pascal Triangle"""
 
 def pascal_triangle(n):
+    """Pascal class"""
     if n <= 0:
         return []
 
     triangle = []
     for i in range(n):
-        row = [1]  # First element of each row is always 1
+        row = []  # First element of each row is always 1
         for j in range(1, i):
             # Calculating the elements of the row using the previous row
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
@@ -15,5 +17,3 @@ def pascal_triangle(n):
         triangle.append(row)
 
     return triangle
-n = 4
-print(pascal_triangle(n))
